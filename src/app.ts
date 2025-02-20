f424eca21cc77af71817f671846bd268f576fc83
PeZunino <pedrozsantos@hotmail.com>
2025-02-20 15:55:34 -0300

    feat(error): global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import fastify from 'fastify';
+import { ZodError } from 'zod';
 import { organizationsRoutes } from './controllers/organizations/routes';
 import { petsRoutes } from './controllers/pets/routes';
 
@@ -7,3 +8,19 @@ export const app = fastify();
 app.register(organizationsRoutes);
 
 app.register(petsRoutes);
+
+app.setErrorHandler((err, _, response)=>{
+	if(err instanceof ZodError){
+		return response.status(400)
+			.send({
+				message: 'Validation error',
+				issues: err.format()
+			});
+	}
+  
+	console.error(err);
+	
+
+	return response.status(500)
+		.send({message: 'Internal server error'});
+});
